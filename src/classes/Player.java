73d342eeff3f5e78e42d3b0a791b921698f3b21c
PinHead <apchi877@gmail.com>
2015-11-24 11:42:57 +0200

    new classes and updates

--- src/classes/Player.java
+++ src/classes/Player.java
@@ -1,5 +1,14 @@
 package classes;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+	private String name;
+	private Team team;
+	private int points;
+	
+	private ArrayList<Segment> segmentsPublished;
+	private ArrayList<Review> reviewsWrote;
+	
+	
 }
